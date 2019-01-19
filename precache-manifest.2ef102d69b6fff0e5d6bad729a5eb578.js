7d4dea2441b33e0ccff8d21a267068ac8184e1d2
Aseel Abusal <aseel.abosul@gmail.com>
2019-01-19 19:51:35 +0200

    Updates

---
+++ precache-manifest.2ef102d69b6fff0e5d6bad729a5eb578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a73abdcfa4072db5abd",
+    "url": "/facerecognition/static/js/runtime~main.1a73abdc.js"
+  },
+  {
+    "revision": "af041d74baff3fd647ba",
+    "url": "/facerecognition/static/js/main.af041d74.chunk.js"
+  },
+  {
+    "revision": "00bbdca28158118fce34",
+    "url": "/facerecognition/static/js/1.00bbdca2.chunk.js"
+  },
+  {
+    "revision": "af041d74baff3fd647ba",
+    "url": "/facerecognition/static/css/main.23d1ff8e.chunk.css"
+  },
+  {
+    "revision": "00bbdca28158118fce34",
+    "url": "/facerecognition/static/css/1.23138680.chunk.css"
+  },
+  {
+    "revision": "3a25f33486aef77b6e4c06082e05c665",
+    "url": "/facerecognition/index.html"
+  }
+];
